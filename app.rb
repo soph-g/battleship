8ebbbff15af74d0fa453b94f9a60235c0f6df45b
Terry <terry@terrytilley.com>
2016-07-19 13:53:45 +0100

    Challenges upto 16

--- app.rb
+++ app.rb
@@ -9,8 +9,17 @@ get '/secret' do
   "This is a secret page"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE' alt='cat'/>
-  </div>"
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
